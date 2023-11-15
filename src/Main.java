4609d21f971ccf3b10e6a0350c5b1fee5603ef54
Sean Valiente <129992276+Nitevale@users.noreply.github.com>
2023-11-16 03:02:02 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
-// Main.java
+//
 public class Main {
     public static void main(String[] args) {
-        // Using the factory to create instances
         ElectronicDeviceFactory deviceFactory = new ElectronicDeviceFactory();
 
         ElectronicDevice laptop = deviceFactory.createDevice("Laptop");
